4317b9f9b1c68bddc8fb972c883a747481e1d700
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-11-07 20:03:00 +0530

    add private auth

--- src/server/routers/auth-router.ts
+++ src/server/routers/auth-router.ts
@@ -10,7 +10,27 @@ export const authRouter = router({
   getDatabaseSyncStatus: publicProcedure.query(async ({ c, ctx }) => {
     const auth = await currentUser()
 
-    return c.json({ status: "success" })
+    if (!auth) {
+      return c.json({ isSynced: false })
+    }
+
+    const user = await db.user.findFirst({
+      where: { externalId: auth.id },
+    })
+
+    console.log('USER IN DB:', user);
+
+    if (!user) {
+      await db.user.create({
+        data: {
+          quotaLimit: 100,
+          externalId: auth.id,
+          email: auth.emailAddresses[0].emailAddress,
+        },
+      })
+    }
+
+    return c.json({ isSynced: true })
   }),
 })
 
