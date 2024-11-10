db1d266a56af86aa4b66940cfdd74290e83d16d1
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-11-10 20:53:00 +0530

    add category

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,41 @@
+import { DashboardPage } from "@/components/dashboard-page"
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import { DashboardPageContent } from "./dashboard-page-content"
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const auth = await currentUser()
+
+  if (!auth) {
+    redirect("/sign-in")
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) {
+    return redirect("/welcome")
+  }
+
+  
+
+  return (
+    <>
+      
+
+      <DashboardPage title="Dashboard">
+        <DashboardPageContent />
+      </DashboardPage>
+    </>
+  )
+}
+
+export default Page
